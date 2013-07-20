d7845a154f9605178d9b70e45a40b6e3f0028455
Kasheftin <Kasheftin@gmail.com>
2013-07-20 23:24:30 +0300

    StartMode added while endKey<serverKey

--- src/js/widgets/TrackerPageDebug/main.js
+++ src/js/widgets/TrackerPageDebug/main.js
@@ -620,7 +620,10 @@ define([
 				else if (self.isCurrentlyOnline() && dt > config.dtDiffReply)
 					self.setReplyMode(false);
 			});
-			self.setLiveMode();
+			if (self.endKey() < self.serverKey())
+				self.setStartMode();
+			else
+				self.setLiveMode();
 			self.playerControl.enableOfflineNotification(true);
 		}
 		else
@@ -629,12 +632,16 @@ define([
 
 	TrackerPageDebug.prototype.setLiveMode = function() {
 		if (!this.isOnline() || this.isCurrentlyOnline()) return;
-		this.isCurrentlyOnline(true);
+//		this.isCurrentlyOnline(true);
 		if (this.playerControl) {
 			this.playerState("play");
 			this.playerSpeed(1);
 			this.playerControl.emit("change",this.serverKey());
 		}
+	}
+
+	TrackerPageDebug.prototype.setStartMode = function() {
+		this.playerControl.emit("change",this.startKey());
 	}
 
 	TrackerPageDebug.prototype.setReplyMode = function() {
