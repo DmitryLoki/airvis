cf4ccddc5e72515c639d35bedf02d87d846f5326
Kasheftin <Kasheftin@gmail.com>
2013-08-15 17:12:28 +0300

    unnecessary files deleted

--- src/js/widgets/AppSimple/main.js
+++
@@ -1,8 +0,0 @@
-define(['widget!TrackerPageSimple'], function(TrackerPageSimple) {
-	var App = function(){
-		this.page = new TrackerPageSimple;
-		window.airvisPage = this.page;
-	};
-
-	return App;
-});
